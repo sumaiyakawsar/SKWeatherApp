a76af860096477d56ac841bccca34c95e11c4542
Sumaiya Kawsar <sumaiya.kawsar693@gmail.com>
2022-05-17 18:28:09 +0300

    Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true, weatherApiBaseUrl:'https://weatherapi-com.p.rapidapi.com/current.json',
+  XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue:'weatherapi-com.p.rapidapi.com',
+  XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue:'ceb937ebd0msh0fe7ce095905322p1fa576jsn2a8781e5ce82'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,11 @@
 
 export const environment = {
   production: false,
-  // weatherApiBaseUrl:'https://community-open-weather-map.p.rapidapi.com/weather',
   weatherApiBaseUrl:'https://weatherapi-com.p.rapidapi.com/current.json',
   XRapidAPIHostHeaderName:'X-RapidAPI-Host',
-  // XRapidAPIHostHeaderValue:'community-open-weather-map.p.rapidapi.com',
-    XRapidAPIHostHeaderValue:'weatherapi-com.p.rapidapi.com',
+  XRapidAPIHostHeaderValue:'weatherapi-com.p.rapidapi.com',
   XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
-  // XRapidAPIKeyHeaderValue:'ceb937ebd0msh0fe7ce095905322p1fa576jsn2a8781e5ce82',
-   XRapidAPIKeyHeaderValue:'ceb937ebd0msh0fe7ce095905322p1fa576jsn2a8781e5ce82'
+  XRapidAPIKeyHeaderValue:'ceb937ebd0msh0fe7ce095905322p1fa576jsn2a8781e5ce82'
 };
 
 /*
